ffca27aed63fc1d42ad3d2eec1ddb6304027bede
stefan-misik <mail@stefanmisik.eu>
2015-03-31 19:18:27 +0200

    Added lcd driver files

---
+++ SW/embedded/lcd.c
@@ -0,0 +1,37 @@
+#include "lcd.h"
+
+/**
+ * \brief Set lcd mode to DATA
+ */
+#define LCD_SET_D()		PORTD |= (1 << PORTD2)
+
+/**
+ * \brief Set lcd mode to CONTROL
+ */
+#define LCD_SET_C()		PORTD &= ~(1 << PORTD2);
+
+/******************************************************************************/
+void lcd_init(
+	void
+)
+{
+	/* Set output direction to LCD DC, SCE, LED pin */
+	DDRD |= ( (1 << DDD2) | (1 << DDD4) | (1 << DDD5) );
+	
+	
+}
+
+/******************************************************************************/
+void lcd_set_bl(
+	char bl
+)
+{
+	if(bl)
+	{
+		PORTD |= (1 << PORTD5);
+	}
+	else
+	{
+		PORTD &= ~(1 << PORTD5);
+	}
+}
---
+++ SW/embedded/lcd.h
@@ -0,0 +1,15 @@
+#ifndef LCD_H_
+#define LCD_H_
+
+#include <avr/io.h>
+
+void lcd_init(
+	void
+);
+
+void lcd_set_bl(
+	char bl
+);
+
+
+#endif /* LCD_H_ */
