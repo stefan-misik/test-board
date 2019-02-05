ab28e268c215a2bda3f105f215774534afe44b7f
Stefan Misik <stefan-misik@users.noreply.github.com>
2019-02-05 21:37:22 +0100

    [SW] Added FIFO tests and functionality

--- SW/embedded/tests/fifo.cpp
+++ SW/embedded/tests/fifo.cpp
@@ -3,7 +3,7 @@
 
 #include "../tools/fifo.h"
 
-int main(int ac, char** av)
+int main(int ac, char **av)
 {
     return CommandLineTestRunner::RunAllTests(ac, av);
 }
@@ -12,31 +12,93 @@ TEST_GROUP(Fifo)
 {
     fifo_t m_fifo_object;
     char m_buffer[64];
+    char *m_buffer_start;
 
     TEST_SETUP()
     {
-        for (unsigned n = 0; n < (sizeof(m_buffer) / sizeof(m_buffer[0])); ++ n)
+        m_buffer_start = &(m_buffer[16]);
+
+        for (unsigned n = 0; n < (sizeof(m_buffer) / sizeof(m_buffer[0])); ++n)
         {
             m_buffer[n] = 0;
         }
 
-        fifo_init(&m_fifo_object, &(m_buffer[16]), 8);
+        fifo_init(&m_fifo_object, m_buffer_start, 6);
     }
 };
 
 TEST(Fifo, emptyAfterInitialization)
 {
+    // Prepare
     fifo_init(&m_fifo_object, m_buffer, 32);
-    LONGS_EQUAL(32, fifo_free(&m_fifo_object));
-    LONGS_EQUAL(0, fifo_enqueued(&m_fifo_object));
+    // Test, Check
+    UNSIGNED_LONGS_EQUAL(31, fifo_free(&m_fifo_object));
+    UNSIGNED_LONGS_EQUAL(0, fifo_enqueued(&m_fifo_object));
 }
 
 TEST(Fifo, insertSingleByte)
 {
-    LONGS_EQUAL(1, fifo_write(&m_fifo_object, (const void *)"a", 1));
-    LONGS_EQUAL(7, fifo_free(&m_fifo_object));
-    LONGS_EQUAL(1, fifo_enqueued(&m_fifo_object));
-    BYTES_EQUAL('a', m_buffer[16]);
+    // Prepare
+    // Test, Check
+    UNSIGNED_LONGS_EQUAL(1, fifo_write(&m_fifo_object, (const void *)"a", 1));
+    UNSIGNED_LONGS_EQUAL(4, fifo_free(&m_fifo_object));
+    UNSIGNED_LONGS_EQUAL(1, fifo_enqueued(&m_fifo_object));
+    MEMCMP_EQUAL("a", m_buffer_start, 1);
+}
+
+TEST(Fifo, insertMultipleBytes)
+{
+    // Prepare
+    // Test, Check
+    UNSIGNED_LONGS_EQUAL(3, fifo_write(&m_fifo_object, (const void *)"bcd", 3));
+    UNSIGNED_LONGS_EQUAL(2, fifo_free(&m_fifo_object));
+    UNSIGNED_LONGS_EQUAL(3, fifo_enqueued(&m_fifo_object));
+    MEMCMP_EQUAL("bcd", m_buffer_start, 3);
+}
+
+TEST(Fifo, readSingleByte)
+{
+    char buffer[8];
+    // Prepare
+    fifo_write(&m_fifo_object, "xq", 2);
+    // Test
+    UNSIGNED_LONGS_EQUAL(1, fifo_read(&m_fifo_object, buffer, 1));
+    // Check
+    UNSIGNED_LONGS_EQUAL(4, fifo_free(&m_fifo_object));
+    UNSIGNED_LONGS_EQUAL(1, fifo_enqueued(&m_fifo_object));
+    MEMCMP_EQUAL("x", buffer, 1);
+}
+
+TEST(Fifo, readMultipleBytes)
+{
+    char buffer[8];
+    // Prepare
+    fifo_write(&m_fifo_object, "xqrt", 4);
+    // Test
+    UNSIGNED_LONGS_EQUAL(3, fifo_read(&m_fifo_object, buffer, 3));
+    // Check
+    UNSIGNED_LONGS_EQUAL(4, fifo_free(&m_fifo_object));
+    UNSIGNED_LONGS_EQUAL(1, fifo_enqueued(&m_fifo_object));
+    MEMCMP_EQUAL("xqr", buffer, 3);
+
+}
+
+TEST(Fifo, multipleReadsAndWrites)
+{
+    char buffer_1[8];
+    char buffer_2[8];
+    // Prepare
+    fifo_write(&m_fifo_object, "ym", 2);
+    fifo_read(&m_fifo_object, buffer_1, 1);
+    // Test
+    UNSIGNED_LONGS_EQUAL(2, fifo_write(&m_fifo_object, "no", 2));
+    UNSIGNED_LONGS_EQUAL(2, fifo_read(&m_fifo_object, buffer_2, 2));
+    // Check
+    UNSIGNED_LONGS_EQUAL(4, fifo_free(&m_fifo_object));
+    UNSIGNED_LONGS_EQUAL(1, fifo_enqueued(&m_fifo_object));
+    MEMCMP_EQUAL("y", buffer_1, 1);
+    MEMCMP_EQUAL("mn", buffer_2, 2);
+
 }
 
 /* REQUIREMENTS:
--- SW/embedded/tools/fifo.c
+++ SW/embedded/tools/fifo.c
@@ -8,30 +8,39 @@ void fifo_init(fifo_t * fifo, void * buffer, size_t buffer_length)
     fifo->buffer = buffer;
     fifo->length = buffer_length;
     fifo->head = 0;
+    fifo->tail = 0;
 }
 
 /******************************************************************************/
 size_t fifo_free(const fifo_t * fifo)
 {
-    return fifo->length - fifo->head;
+    return fifo->length - (fifo->head - fifo->tail) - 1;
 }
 
 /******************************************************************************/
 size_t fifo_enqueued(const fifo_t * fifo)
 {
-    return fifo->head;
+    return fifo->head - fifo->tail;
 }
 
 /******************************************************************************/
 size_t fifo_write(fifo_t * fifo, const void * buffer, size_t length)
 {
-    BUF(fifo->buffer)[fifo->head] = BUF(buffer)[0];
-    fifo->head += length;
-    return length;
+    size_t pos;
+    for (pos = 0; pos < length; pos ++)
+    {
+        BUF(fifo->buffer)[fifo->head ++] = BUF(buffer)[pos];
+    }
+    return pos;
 }
 
 /******************************************************************************/
 size_t fifo_read(fifo_t * fifo, void * buffer, size_t length)
 {
-    return 0;
+    size_t pos;
+    for (pos = 0; pos < length; pos ++)
+    {
+        BUF(buffer)[pos] = BUF(fifo->buffer)[fifo->tail ++];
+    }
+    return pos;
 }
